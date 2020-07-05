2874d28606e5d7871be9d79da486649d6b71df8e
Webmets <mets.radboud@gmail.com>
2020-07-05 17:02:43 +0200

    fix indentation

--- app/Events/ImageUploaded.php
+++ app/Events/ImageUploaded.php
@@ -13,19 +13,19 @@ use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
 class ImageUploaded implements ShouldBroadcast { 
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+  use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    public $queue = 'high';
+  public $queue = 'high';
 
-    public $image;
-    private $user;
+  public $image;
+  private $user;
 
-    public function __construct(Image $image, User $user) {
-      $this->image = $image;
-      $this->user = $user;
-    }
+  public function __construct(Image $image, User $user) {
+    $this->image = $image;
+    $this->user = $user;
+  }
 
-    public function broadcastOn() {
-        return new PrivateChannel('image.uploaded.' . $this->user->id);
-    }
+  public function broadcastOn() {
+    return new PrivateChannel('image.uploaded.' . $this->user->id);
+  }
 }
